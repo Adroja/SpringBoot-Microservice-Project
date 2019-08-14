9d6183bd203424995cb79c74318417621743f0e5
Manish Adroja <mhadroja@gmail.com>
2019-08-14 21:24:13 +0530

    added get post functionalities with mysql

--- Spring-Eureka-Client-Student-Service/src/main/java/com/example/accelerate/SpringEurekaClientUserService/SpringEurekaClientUserServiceApplication.java
+++ Spring-Eureka-Client-Student-Service/src/main/java/com/example/accelerate/SpringEurekaClientUserService/SpringEurekaClientUserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.accelerate.SpringEurekaClientStudentService;
+package com.example.accelerate.SpringEurekaClientUserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class SpringEurekaClientStudentServiceApplication {
+public class SpringEurekaClientUserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringEurekaClientStudentServiceApplication.class, args);
+		SpringApplication.run(SpringEurekaClientUserServiceApplication.class, args);
 	}
 
 }
